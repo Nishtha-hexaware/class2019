d08711dcfec19b2615cd675e8f50f8d5800063c4
Nishtha-hexaware <46744983+Nishtha-hexaware@users.noreply.github.com>
2019-03-01 09:48:40 +0530

    Add files via upload

---
+++ leave-details.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { LeaveDetails } from '../leave-details';
+import {Observable} from 'rxjs';
+import {LeaveDetailsService} from '../leave-details.service';
+import {Router} from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-leave-details',
+  templateUrl: './leave-details.component.html',
+  styleUrls: ['./leave-details.component.css']
+})
+export class LeaveDetailsComponent implements OnInit {
+
+  leavedetails : Observable<LeaveDetails>;
+  empData : Observable<Employee[]>;
+  empno : number;
+  mgr : number;
+  showButton : boolean;
+
+  setClickRowDup(leaId, empId) { 
+    this.showButton=true;
+    alert(leaId);
+    alert("Employ Id  " +empId);
+    localStorage.setItem("leavid",leaId);
+    localStorage.setItem("lempid",empId);
+
+  }
+  doApproveDeny() {
+    alert("Redirecting...");
+    this._router.navigate(["/ApproveDeny"]);
+
+  }
+  constructor(private leaveDetailsService : LeaveDetailsService, private empService : EmployeeService,
+       private _router : Router) { 
+         this.showButton=false;
+    this.empno=parseInt(localStorage.getItem("empId"));
+    this.leavedetails = leaveDetailsService .searchLeave(this.empno);
+    this.mgr=parseInt(localStorage.getItem("empMgrId")); 
+    this.empData = empService.getEmployee();
+    }
+  ngOnInit() {
+  }
+
+  
+
+}
