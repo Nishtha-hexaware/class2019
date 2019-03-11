81a9f99d9d355f0a6224da778acdb014214658e8
Ishant <IshantG@hexaware.com>
2019-03-11 17:57:20 +0530

    ch

--- app/sortdata.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortdataPipe } from './sortdata.pipe';
-
-describe('SortdataPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortdataPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
